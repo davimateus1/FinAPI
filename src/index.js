88a15c0e5b22bdb7dc02f1be2e23a43d271c9288
Davi Mateus <davi.2020109493@unicap.br>
2022-12-08 17:33:56 -0300

    feat: create user

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const { v4: uuidV4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (req, res) => {
+  const { name, cpf } = req.body;
+  const id = uuidV4();
+
+  customers.push({
+    name,
+    cpf,
+    id,
+    statement: [],
+  });
+
+  return res.send(201).send();
+});
 
 app.listen(3333, () => {
   console.log("Server is running on port 3333");
