0717f761b4c3283f53c96e0749eec45432e2a9af
Davi Mateus <davi.2020109493@unicap.br>
2022-12-08 17:41:12 -0300

    feat: validate cpf before create user

--- src/index.js
+++ src/index.js
@@ -9,12 +9,19 @@ const customers = [];
 
 app.post("/account", (req, res) => {
   const { name, cpf } = req.body;
-  const id = uuidV4();
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    res.status(400).json({ error: "Customer already exists!" });
+  }
 
   customers.push({
     name,
     cpf,
-    id,
+    id: uuidV4(),
     statement: [],
   });
 
