32b0c94c89cdb060058346c99b2f34205c1e5616
Davi Mateus <davi.2020109493@unicap.br>
2022-12-10 15:19:27 -0300

    feat: update account

--- src/index.js
+++ src/index.js
@@ -54,6 +54,20 @@ app.post("/account", (req, res) => {
   return res.send(201).send();
 });
 
+app.get("/account", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  return res.json(customer);
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (req, res) => {
+  const { name } = req.body;
+  const { customer } = req;
+
+  customer.name = name;
+
+  return res.status(201).send();
+});
+
 app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
   const { amount, description } = req.body;
   const { customer } = req;
